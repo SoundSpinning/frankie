629a28396bb093802e29580e60ba006574d6a223
SoundSpinning <SoundSpinning@hotmail.com>
2026-08-21 11:02:15 +0100

    contrast tweaks

--- js/main.js
+++ js/main.js
@@ -106,23 +106,23 @@ function initVideo() {
 // Audio
 // Play/Pause tracks with single play click & when ended it moves to next track
 // Note: it should work on iOS devices
-function initAudio() {
-    document.addEventListener('play', function (e) {
-        var allTracks = document.getElementsByTagName('audio');
-        var indexT = Array.prototype.indexOf.call(allTracks, e.target);
-        for (var i = 0, len = allTracks.length; i < len; i++) {
-            if (allTracks[i] != e.target) {
-                allTracks[i].pause(); // stop All Sounds except play one
-            }
-        }
-        // this moves onto next track when current one ends
-        e.target.addEventListener('ended', function(){            
-            if (indexT != null) {
-                if (indexT == allTracks.length-1) {
-                    indexT = -1;
-                }
-            allTracks[indexT+1].play();
-            }
-        });
-    }, true);
-}
+// function initAudio() {
+//     document.addEventListener('play', function (e) {
+//         var allTracks = document.getElementsByTagName('audio');
+//         var indexT = Array.prototype.indexOf.call(allTracks, e.target);
+//         for (var i = 0, len = allTracks.length; i < len; i++) {
+//             if (allTracks[i] != e.target) {
+//                 allTracks[i].pause(); // stop All Sounds except play one
+//             }
+//         }
+//         // this moves onto next track when current one ends
+//         e.target.addEventListener('ended', function(){            
+//             if (indexT != null) {
+//                 if (indexT == allTracks.length-1) {
+//                     indexT = -1;
+//                 }
+//             allTracks[indexT+1].play();
+//             }
+//         });
+//     }, true);
+// }
